21c3a8a6eb5caab10b42c4c03eb75f26ca8b080a
Andre Lopez <andre@barrelstrengthdesign.com>
2017-09-14 11:44:44 -0400

    Adds modal window to see errors

--- src/resources/js/enupalbackup.js
+++ src/resources/js/enupalbackup.js
@@ -6,13 +6,27 @@
 	var EnupalBackup = Garnish.Base.extend({
 
 		options: null,
+		modalError1: null,
 
 		/**
 		 * The constructor.
 		 */
 		init: function()
 		{
+			this.addListener($("#showError1"), 'activate', 'showError1');
+		},
 
+		showError1: function(option)
+		{
+			if (this.modalError1)
+			{
+				this.modalError1.show();
+			}
+			else
+			{
+				var $div = $('#error1');
+				this.modalError1 = new Garnish.Modal($div);
+			}
 		},
 
 	});
